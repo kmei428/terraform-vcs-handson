9cbf1b1475a3d96faf9a23f41ca59b0bef80a733
kmei428 <80740233+kmei428@users.noreply.github.com>
2025-10-22 18:15:57 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,9 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami           = "ami-0c3fd0f5d33134a76" # Amazon Linux 2 (東京リージョン)
   instance_type = "t3.micro"
+  subnet_id              = "subnet-0b36929da7db0b954"
+  associate_public_ip_address = true
   tags = {
-    Name = "handson-{ご自身のお名前}-instance-day2"
+    Name = "handson-{ご自身のお名前}-instance"
   }
 }
